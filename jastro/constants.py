c63caef70ebf89f4f20f2739f6920644a9486c3c
James McCormac <jmccormac001@gmail.com>
2018-07-27 10:59:04 +0100

    updates some constants

--- jastro/constants.py
+++ jastro/constants.py
@@ -6,19 +6,38 @@ class Constants(object):
     """
     Common suite of useful constants in astronomy
 
-    Solar mass, radius and luminosity values are from
-    Harmanec & Prsa arXiv:1106.1508v2
+    Radii, masses etc for solar system bodies are taken
+    from Mamajek et al. 2015
+    https://arxiv.org/pdf/1510.07674.pdf
     """
+    # the 2014 CODATA value is G = 6.67408 (±0.00031) × 10−11 m3 kg−1 s−2
     G = 6.67408E-11 # m3 kg^-1 s^-2
-    Msun = 1.988547E30 # kg
-    Rsun = 6.95508E8 # m
-    Lsun = 3.846E26 # W
-    Mjup = 1.898E27 # kg
-    Rjup = 69911000 # m
+    # Sun
+    GMsun = 1.3271244E20 # m^3 s^-2
+    Msun = GMsun/G # kg
+    Rsun = 6.957E8 # m
+    Lsun = 3.828E26 # W
+    Ssun = 1361. # W m^-2
+    Teffsun = 5772. # K
+    # Jupiter
+    GMjup = 1.2668653E17 # m^3 s^-2
+    Mjup = GMjup/G # kg
+    Rpjup = 6.6854E7 # m - polar radius
+    Rejup = 7.1492E7 # m - equatorial radius
+    Rjup =  Rejup # if not specified, we use the equatorial
+    # Earth
+    GMe = 3.986004E14 # m^3 s^-2
+    Me = GMe/G # kg
+    Rpe = 6.3568E6 # m - polar radius
+    Ree = 6.3781E6 # m - equatorial radius
+    Re =  Ree # if not specified, we use the equatorial
+    # Misc astronomy
+    AU = 1.496E11 # m
+    # Time
+    sec_d = 24*60*60 # s
+    # Physics
     Mproton = 1.6726219E-27 # kg
     boltzmann = 1.38064852E-23 # m^2 kg s^-2 K^-1
-    AU = 1.496E11 # m
-    sec_d = 24*60*60 # s
 
     def _list_constants():
         """
