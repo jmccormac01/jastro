b03270f1e9668355a1d90456b8db297a62302890
James McCormac <jmccormac001@gmail.com>
2019-01-18 11:46:43 +0000

    adds binning of errorbars

--- jastro/lightcurves.py
+++ jastro/lightcurves.py
@@ -1,7 +1,13 @@
 """
 A series of functions to handle light curves
 """
+from collections import (
+    OrderedDict,
+    defaultdict
+    )
 import numpy as np
+
+# pylint: disable=invalid-name
 
 def lc_mags_to_flux(mags, mags_err):
     """
@@ -26,12 +32,79 @@ def phase_times(times, epoch, period, phase_offset=0.0):
     """
     return (((times - epoch)/period)+phase_offset)%1
 
-def pc_bin(time, mmi, bin_width, clip_empty_bins=True):
+def pc_bin(time, flux, error, bin_width, clip_empty_bins=True):
+    """
+    """
     bin_edges = np.arange(np.min(time), np.max(time), bin_width)
     digitized = np.digitize(time, bin_edges)
     binned_time = (bin_edges[1:] + bin_edges[:-1]) / 2
-    binned_mmi = np.array([np.nan if len(mmi[digitized == i]) == 0 else mmi[digitized == i].mean() for i in range(1, len(bin_edges))])
+    binned_flux = np.array([np.nan if len(flux[digitized == i]) == 0 else flux[digitized == i].mean() for i in range(1, len(bin_edges))])
+    binned_error = np.array([np.nan if len(error[digitized == i]) == 0 else np.sqrt(np.sum(error[digitized == i]**2.))/len(error[digitized==i]) for i in range(1, len(bin_edges))])
     if clip_empty_bins:
-        binned_time = binned_time[~np.isnan(binned_mmi)]
-        binned_mmi = binned_mmi[~np.isnan(binned_mmi)]
-    return (binned_time, binned_mmi)
+        binned_time = binned_time[~np.isnan(binned_flux)]
+        binned_flux = binned_flux[~np.isnan(binned_flux)]
+        binned_error = binned_error[~np.isnan(binned_flux)]
+    return (binned_time, binned_flux, binned_error)
+
+def extract_nights_with_transits(times, flux, err, epoch,
+                                 period, t14, transit_type='full'):
+    """
+    Take in a time series and ephmeris and
+    return only the nights with transits
+    """
+    # split the data into separate transits
+    nights = OrderedDict()
+    skip, hit = 0, 0
+    phase_lim = (t14/2.)/period
+    for i, t in enumerate(times):
+        ph = (t - epoch)/period
+        if ph%1 >= 0.90 or ph%1 <= 0.1:
+            try:
+                nights[round(ph)]['hjd'].append(t)
+                nights[round(ph)]['flux'].append(flux[i])
+                nights[round(ph)]['phase'].append(ph%1)
+                nights[round(ph)]['error'].append(err[i])
+            except KeyError:
+                nights[round(ph)] = defaultdict(list)
+                nights[round(ph)]['hjd'].append(t)
+                nights[round(ph)]['flux'].append(flux[i])
+                nights[round(ph)]['phase'].append(ph%1)
+                nights[round(ph)]['error'].append(err[i])
+            hit += 1
+        else:
+            skip += 1
+
+    if transit_type == 'partial':
+        # Do a second pass to see if none of the data points are in transit
+        print('Checking full and partial transits...')
+        to_delete = []
+        for night in nights:
+            hits = 0
+            for p in nights[night]['phase']:
+                if p >= 1-phase_lim or p <= phase_lim:
+                    hits += 1
+            if hits == 0:
+                print("Delete transit {}, no points within phase_lim".format(night))
+                to_delete.append(night)
+    elif transit_type == 'full':
+        print('Checking for only full transits...')
+        to_delete = []
+        for night in nights:
+            hits_l = 0
+            hits_p = 0
+            for p in nights[night]['phase']:
+                if p <= 1-phase_lim and p > 0.5:
+                    hits_l += 1
+                elif p >= phase_lim and p < 0.5:
+                    hits_p += 1
+            if hits_l < 10 or hits_p < 10:
+                to_delete.append(night)
+    else:
+        return None
+
+    # do the final cut
+    print("N nights pre-clean-up: {}".format(len(nights)))
+    for d in to_delete:
+        del nights[d]
+    print("N nights post-clean-up: {}".format(len(nights)))
+    return nights
